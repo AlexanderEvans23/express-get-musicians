274f7a87d93eb9c9dc43bb0a17993a32ab103939
Alex Evans <aevans@quiktrip.com>
2024-02-21 11:12:33 -0600

    added endpoint functions

--- index.test.js
+++ index.test.js
@@ -8,10 +8,15 @@ const { db } = require('./db/connection');
 const { Musician } = require('./models/index')
 const app = require('./src/app');
 const seedMusician = require("./seedData");
+const Test = require('supertest/lib/test');
+const { hasUncaughtExceptionCaptureCallback } = require('process');
 
 
 describe('./musicians endpoint', () => {
-    // Write your tests here
+    test("testing musician endpoint", async () => {
+        const response = await request(app).get('/musicians');
+        expect(response.statusCode).toBe(200);
+    })
     
     
 
