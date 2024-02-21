5687ee73a59a6ac6b5dcbac1835448142e76e3bb
Alex Evans <aevans@quiktrip.com>
2024-02-21 13:22:56 -0600

    added tests

--- index.test.js
+++ index.test.js
@@ -9,7 +9,6 @@ const { Musician } = require('./models/index')
 const app = require('./src/app');
 const seedMusician = require("./seedData");
 const Test = require('supertest/lib/test');
-const { hasUncaughtExceptionCaptureCallback } = require('process');
 
 
 describe('./musicians endpoint', () => {
@@ -18,7 +17,63 @@ describe('./musicians endpoint', () => {
         expect(response.statusCode).toBe(200);
     })
     
-    
+    test("get/rest returns proper array", async () => {
+        const response = await request(app).get("/musicians");
+        expect(Array.isArray(response.body)).toBe(true)
+    })
+
+    test("get/rest returns proper number of array elements", async () => {
+        const response = await request(app).get("/musicians");
+        expect(response.body.length).toBe(restQuantity)
+    })
+
+    test("get/ rest returns correct data", async () => {
+        const response = await request(app).get("/musicians");
+        expect(response.body).toContainEqual(
+            expect.objectContaining({
+                id: 1,
+                name: 'AppleBees',
+                instrument: 'Texas'
+            })
+        )
+    })
+
+    test("route returns proper spot in array", async () => {
+        const response = await request(app).get("/musicians/1");
+        expect(response.body).toEqual(
+            expect.objectContaining({
+                id: 1,
+                name: 'AppleBees',
+                instrument: 'Texas'
+            })
+        )
+    })
+
+
+    test("route places new post at end of array ", async () => {
+        await request(app)
+        .post("/musicians")
+        .send({name: "asd", instrument: "qwe"});
+        const musician = await Musician.findByPk(7);
+        expect(musician.name).toEqual("asd")
+    })
+
+    test("route updates proper spot in array", async () => {
+        const response = await request(app)
+        .put("/musicians/1")
+        .send({name: "asd", instrument: "qwe"});
+        const musician = await Musician.findByPk(1);
+        expect(musician.name).toEqual('asd')
+        
+    })
+
+
+    test("route properly deletes spot in array", async () => {
+        const response = await request(app)
+        .delete("/musicians/1")
+        let musician = await Musician.findAll()
+        expect(musician.length).toEqual(restQuantity)
+    })
 
 
 
--- src/app.js
+++ src/app.js
@@ -4,6 +4,10 @@ const { Musician } = require("../models/index")
 const { db } = require("../db/connection")
 
 const port = 3000;
+app.use(express.json())
+app.use(express.urlencoded())
+
+
 
 //TODO: Create a GET /musicians route to return all musicians 
 
@@ -11,6 +15,33 @@ app.get("/musicians", async (req, res) =>{
     let foundMusic = await Musician.findAll();
     res.json(foundMusic)
 })
+
+app.get("/musicians/:id", async (req, res) => {
+    let id = req.params.id;
+    let music = await Musician.findByPk(id)
+    res.json(music)
+})
+
+app.post("/musicians", async (req, res) => {
+    let newMusic = await Musician.create(req.body);
+    res.json(newMusic)
+})
+
+app.put('/musicians/:id', async (req, res) => {
+    let id = req.params.id;
+    let newMusic = await Musician.update(id);
+    res.json(newMusic)
+})
+
+app.delete('/musicians/:id', async (req, res) => {
+    let id = req.params.id;
+    let deleted = await Musician.destroy({where: {
+        id: id
+    },
+    });
+    res.json(deleted)
+})
+
 
 
 
